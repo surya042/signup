25e5e0d36d900238e72eda8a1f0cce0ef98316ec
RS SURYA <rssurya19@siesgst.ac.in>
2024-01-15 20:33:18 +0530

    first commit

---
+++ SignUpForm-HTML_NodeJS_MongoDB-main/index.js
@@ -0,0 +1,58 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var firstName = req.body.firstNameName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    var orgCategory = req.body.orgCategory;
+    var companyName = req.body.companyName;
+
+    var data = {
+        "firstName": firstName,
+        "lastName": lastName,
+        "email" : email,
+        "orgCategory": orgCategory,
+        "companyName" : companyName
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('signup_success.html')
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
+
+
+console.log("Listening on PORT 3000");
